150ca00766badee5b9d698e88820b06a060b94b4
Murilo Sena <102439644+Sena2K@users.noreply.github.com>
2024-06-02 23:45:27 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,35 +1,10 @@
 require_relative 'maquina-turing-universal'
-require_relative 'mt-anbncn'
 require_relative 'mt-anbn'
-require_relative 'mt-multiplicacao'
+
 
 puts "====================================================================="
 
 entrada = MtCodificada.linker + '$' + MtCodificada.codificacao_cadeia
-mt = MTU.new
-
-puts "Entrada:\n #{entrada}"
-puts "Decidiu? #{mt.processar(entrada)}"
-puts "Fita Resultante:\n #{mt.fita}"
-puts "Cursor parou em #{mt.cursor}"
-puts "Cursor no estado #{mt.estado}"
-puts "Cursor está lendo \"#{mt.fita[mt.cursor]}\""
-
-puts "====================================================================="
-
-entrada = MtKuroda.linker + '$' + MtKuroda.codificacao_cadeia
-mt = MTU.new
-
-puts "Entrada:\n #{entrada}"
-puts "Decidiu? #{mt.processar(entrada)}"
-puts "Fita Resultante:\n #{mt.fita}"
-puts "Cursor parou em #{mt.cursor}"
-puts "Cursor no estado #{mt.estado}"
-puts "Cursor está lendo \"#{mt.fita[mt.cursor]}\""
-
-puts "====================================================================="
-
-entrada = MtMultiplicacao.linker + '$' + MtMultiplicacao.codificacao_cadeia
 mt = MTU.new
 
 puts "Entrada:\n #{entrada}"
